799cfc614cbb63c0e4fb91665c695f411ffa238e
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-12-09 14:21:55 +0900

    Day7_Commit

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,21 +1,83 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sample : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
-        RotateAsync();
+        StartCoroutine(ExecuteAsync());
     }
 
-    IEnumerator RotateAsync()
+    private IEnumerator ExecuteAsync()
     {
-        Debug.Log("RotateAsync");
+        Debug.Log("ExecuteAsync: Begin");
+
         while (true)
         {
-            transform.Rotate(0, 0, 1);
+            // X軸の回転を2秒間
+            yield return RotateAsync(Vector3.right, 2);
+
+            // 5秒間待機する・またはクリックされるのを待つ
+            yield return WhenAny(new WaitForSeconds(5), StartCoroutine(WaitMouseButton(0)));
+
+            // Y軸の回転を2秒間
+            yield return RotateAsync(Vector3.up, 2);
+
+            // 5秒間待機する・またはクリックされるのを待つ
+            yield return WhenAny(new WaitForSeconds(5), StartCoroutine(WaitMouseButton(0)));
+
+            // Z軸の回転を2秒間
+            yield return RotateAsync(Vector3.forward, 2);
+
+            // 5秒間待機する・またはクリックされるのを待つ
+            yield return WhenAny(new WaitForSeconds(5), StartCoroutine(WaitMouseButton(0)));
+        }
+
+        // Debug.Log("ExecuteAsync: End");
+    }
+
+    private IEnumerator RotateAsync(Vector3 eulers, float duration)
+    {
+        Debug.Log($"RotateAsync: Begin eulers={eulers}, duration={duration}");
+        var t = 0F;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.Rotate(eulers);
             yield return null;
         }
+        Debug.Log("ExecuteAsync: End");
+    }
+
+    private IEnumerator WaitClick(float timeout)
+    {
+        Debug.Log($"WaitClick: Begin timeout={timeout}");
+        var t = 0F;
+        while (t < timeout)
+        {
+            t += Time.deltaTime;
+            yield return null;
+            if (Input.GetMouseButton(0)) { break; }
+        }
+        Debug.Log($"WaitClick: End");
+    }
+
+    private IEnumerator WaitMouseButton(int button)
+    {
+        while (!Input.GetMouseButtonDown(button)) { yield return null; }
+    }
+    private IEnumerator Then(YieldInstruction c, Action completed)
+    {
+        yield return c;
+        completed();
+    }
+
+    private IEnumerator WhenAny(YieldInstruction c1, YieldInstruction c2)
+    {
+        var result = false; // c1 または c2 が終わると true になる
+        StartCoroutine(Then(c1, () => result = true));
+        StartCoroutine(Then(c2, () => result = true));
+        while (!result) { yield return null; }
     }
 }
