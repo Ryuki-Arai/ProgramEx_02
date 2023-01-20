6b751ff499e35e1b9655e3ce46d5ae72209d6381
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-01-20 14:16:36 +0900

    Day9_Commit

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -78,4 +78,55 @@ public class Sample : MonoBehaviour
     {
         return Input.GetMouseButtonDown(0);
     }
+
+    /*private IEnumerator RunAsync()
+    {
+        while (true)
+        {
+            Debug.Log("マウスのボタン入力を待ちます");
+            yield return WaitForMouseButtonDown(out var awaiter);
+
+            // Awaiter の終了後は、必ず結果が保証されている
+            Debug.Log($"マウスの{awaiter.Result}ボタンが押されました");
+            yield return null;
+        }
+    }
+
+    public IEnumerator WaitForMouseButtonDown(out IAwaiter<int> awaiter)
+    {
+        var awaiterImpl = new Awaiter<int>();
+        var e = WaitForMouseButtonDown(awaiterImpl);
+        awaiter = awaiterImpl;
+        return e;
+    }
+
+    private IEnumerator WaitForMouseButtonDown(Awaiter<int> awaiter)
+    {
+        // どのマウスボタンが押されたのか、結果を返したい。
+        while (true)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                if (Input.GetMouseButtonDown(i))
+                {
+                    awaiter.SetResult(i); // 処理を終了・結果を設定
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }//*/
+    public interface IAwaiter<T> // 結果を受け取る側のためのインターフェイス
+    {
+        /// <summary>
+        /// 処理が終了したかどうか。
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// 処理の結果。
+        /// </summary>
+        T Result { get; }
+    }
 }
